08879940fca33dd987abb10530ea6aa49c0423a3
wcordeiro <wilson.cordeiro@outlook.com>
2015-04-02 11:21:06 -0300

    Problema 2
    SPOJ

---
+++ Roteiro 0/FATORIA2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ 
+int fact (int n){
+    if (n == 1 || n == 0){
+       return 1;
+   }
+   else 
+      return n * fact(n-1);
+}
+ 
+ 
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if (n > 12){
+       return 0;
+   }
+   printf("%d",fact(n));
+   return 0;
+}

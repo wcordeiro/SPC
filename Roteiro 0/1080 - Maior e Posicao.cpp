c6721869412fd217c8e664d9436d107492c58d99
wcordeiro <wilson.cordeiro@outlook.com>
2015-04-02 11:36:54 -0300

    Problema 5
    URI - 1080

---
+++ Roteiro 0/1080 - Maior e Posicao.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+ 
+ 
+int main (){
+  int aux;
+  int max = 0;
+  int pos=1;
+  int i;
+  for ( i =1; i < 11; i++ ){
+    scanf("%d",&aux);
+    if( aux > max){
+        max = aux;
+        pos = i;
+    }
+  }
+  printf("%d\n%d\n",max,pos);
+ 
+  return 0;
+}
+
+
+
